93fb4720bb7a9489ee85c2848580f135001bd66d
Le Huynh Minh Tri <lehuynhminhtri09082003@gmail.com>
2025-03-04 13:25:56 +0700

    Add Querry DB

--- app/src/main/java/com/example/final_project_group5/Welcome.java
+++ app/src/main/java/com/example/final_project_group5/Welcome.java
@@ -7,12 +7,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
 import com.example.final_project_group5.dao.UsersDAO;
+import com.example.final_project_group5.entity.AppDatabase;
+import com.example.final_project_group5.entity.AppExecutors;
+import com.example.final_project_group5.entity.Users;
+
+import java.util.List;
 
 public class Welcome extends AppCompatActivity {
     Button btnGetStarted;
     TextView tvHaveAccount;
     private UsersDAO usersDAO;
+
+    AppDatabase appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +44,13 @@ public class Welcome extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-//        this.deleteDatabase("electronics_store.db");
-
+        this.deleteDatabase("electronics_store.db");
+        appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "electronics_store.dn").build();
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                List<Users> users = appDatabase.usersDao().getAllUsers();
+            }
+        });
     }
 }
