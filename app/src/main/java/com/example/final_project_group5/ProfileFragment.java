96141d038254b009e5f1fb26d372b9a3881bf346
VuHo <vuho3244@gmail.com>
2025-03-14 12:33:48 +0700

    pull new code for coding

--- app/src/main/java/com/example/final_project_group5/ProfileFragment.java
+++ app/src/main/java/com/example/final_project_group5/ProfileFragment.java
@@ -1,64 +1,67 @@
 package com.example.final_project_group5;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView textName, textEmail, textAddress, textPhone;
+    private ImageView imageProfile;
+    private Button btnEdit;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Ánh xạ các view từ XML
+        textName = view.findViewById(R.id.textView6);
+        textEmail = view.findViewById(R.id.textView10);
+        textAddress = view.findViewById(R.id.textView11);
+        textPhone = view.findViewById(R.id.textView12);
+        imageProfile = view.findViewById(R.id.imageProfile);
+        btnEdit = view.findViewById(R.id.btnEdit);
+
+        // Gọi hàm lấy dữ liệu từ API
+        loadUserProfile();
+    }
+
+    private void loadUserProfile() {
+        // TODO: Viết API call để lấy dữ liệu profile từ server
+        // Ví dụ dữ liệu giả lập:
+        String name = "John Doe";
+        String email = "johndoe@example.com";
+        String address = "123 Main Street, City";
+        String phone = "+1234567890";
+
+        // Set dữ liệu lên UI
+        textName.setText(name);
+        textEmail.setText(email);
+        textAddress.setText(address);
+        textPhone.setText(phone);
+
+        // Nếu có ảnh từ API, có thể dùng Glide/Picasso để load
+        // Glide.with(this).load(profileImageUrl).into(imageProfile);
     }
 }
