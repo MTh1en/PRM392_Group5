f731af3c65d5d8a8b7cfed8679e09d7bddd270c0
MTh1en <mt12122003@gmail.com>
2025-03-20 12:05:57 +0700

    fix/fix header userdashboard

--- app/src/main/java/com/example/final_project_group5/fragment/user/OrderFragmentUser.java
+++ app/src/main/java/com/example/final_project_group5/fragment/user/OrderFragmentUser.java
@@ -47,7 +47,7 @@ public class OrderFragmentUser extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_order_user, container, false);
         if (getArguments() != null) {
             userId = getArguments().getString("USER_ID");
             Log.d("CartFragment", "onCreate - Received userId from Bundle: " + userId); // Thêm log này
