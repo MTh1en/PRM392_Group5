ee1161f45f9cb475cf885eb8f1fa8e329df102d8
MTh1en <mt12122003@gmail.com>
2025-03-24 12:02:55 +0700

    fea/show map, view current location, show shop location, show distance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(fileTree(mapOf(
+        "dir" to "libs",
+        "include" to listOf("*.aar", "*.jar"),
+        "exclude" to listOf("*mock*.jar")
+    )))
+
 }
